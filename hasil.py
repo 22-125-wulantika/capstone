ae5b32e15692e1c1fd21925f4a756a7e696b786a
22-125-wulantika <wullkaa37@gmail.com>
2025-04-21 12:58:26 +0700

    Update hasil.py

--- hasil.py
+++ hasil.py
@@ -70,7 +70,11 @@ if any([filter_price, filter_rating, filter_ram, filter_rom, filter_camera, filt
         rekomendasi = data.iloc[top_indices]
         rekomendasi['No'] = range(1, len(rekomendasi) + 1)
 
-        st.dataframe(rekomendasi[['No', 'Brand', 'Type', 'Price', 'Ratings', 'RAM (GB)', 'ROM (GB)', 'Camera', 'Battery']])
+        st.dataframe(
+    rekomendasi[['No', 'Brand', 'Type', 'Price', 'Ratings', 'RAM (GB)', 'ROM (GB)', 'Camera', 'Battery']],
+    use_container_width=True,
+    hide_index=True
+)
     else:
         st.warning("❌ Tidak ada smartphone yang sesuai dengan kriteria filter Anda.")
 else:
