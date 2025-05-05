6f17477fc2ccdff5c31647994ab4092f73db8fc8
22-125-wulantika <wullkaa37@gmail.com>
2025-05-05 12:41:23 +0700

    Update hasil.py

--- hasil.py
+++ hasil.py
@@ -4,7 +4,7 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics.pairwise import cosine_similarity
 
 # Load dataset
-df = pd.read_excel("data.xlsx")
+df = pd.read_excel("data_smartphones.xlsx")
 
 # Preprocessing kolom Camera
 if "Camera" in df.columns:
